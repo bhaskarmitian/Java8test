41de82eecd4020499c0f9e171f5a3177ae1b3a80
Bhaskar <36102255+bhaskarmitian@users.noreply.github.com>
2022-07-04 17:49:55 +0530

    failfast and failsafe

--- src/main/java/com/datastructure/linklist/Runner.java
+++ src/main/java/com/datastructure/linklist/Runner.java
@@ -5,14 +5,14 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		LinkedList list = new LinkedList();
-		list.insert(18);
-		list.insert(45);
-		list.insert(12);
-		list.insertAtStart(25);
+		list.insert(10);
+		list.insert(20);
+		list.insert(30);
+		//list.insertAtStart(5);
 		
-		list.insertAt(0, 55);
+		//list.insertAt(0, 55);
 		
-		list.deleteAt(2);
+		//list.deleteAt(2);
 		
 		list.show();
 	}
